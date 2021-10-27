06bf7e72e5df6ece5471487743ce50c3acbbd72f
yudhistira.yoga <yudhistiray198@gmail.com>
2021-10-28 00:36:35 +0700

    Add unit testing

---
+++ test/unit_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kriya_test/controller/product/list/product_list.dart';
+import 'package:kriya_test/controller/product/qty/product_qty.dart';
+
+void main() async {
+  test('''
+  Check data from API is not empty''', () {
+    final controller = ProductListController();
+    expect(controller.state, controller.state?.isNotEmpty);
+  });
+
+  test('''check Functionality ''', () async {
+    final controller = ProductQtyController();
+    expect(controller.qty.value, 0);
+
+    controller.increment;
+    expect(controller.qty.value, 1);
+
+    controller.increment;
+    expect(controller.qty.value, 2);
+
+    controller.decrement;
+    expect(controller.qty.value, 1);
+  });
+}
